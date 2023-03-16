f16daef053121f1e3adfca24e02ebbefc9184b6c
juan <jdgonzalez2017@hotmail.com>
2023-03-15 22:13:49 -0300

    agregando task3

--- js/detail.js
+++ js/detail.js
@@ -172,109 +172,46 @@ var data = {
     ]
   };
 
-var data = data;
-const fechaActual = data.currentDate;
-const events = data.events;
 
-// Constantes capturadas y variables
-const contenedor = document.getElementById('items-cards-upcoming')
-const contenedorChecks = document.getElementById('checkbox-items')
-const input = document.querySelector('input')
+let query = location.search
+let params = new URLSearchParams(query)
+let idParams = params.get("id")
+
+let profile = data.events.find(info => info._id == idParams)
 
 
+const container = document.getElementById("container-detail");
+console.log("estoy en el container-detail", container);
 
-// Eventos
-input.addEventListener('input',superFiltro)
+let html = "";
 
-contenedorChecks.addEventListener('change',superFiltro)
+html += `
 
+<div class="card mb-3 detail-container " style="max-width: 740px; max-height: 740px">
+        <div class="row g-0 detail">
+          <div class="col-md-4">
+            <img
+              src="${profile.image}"
+              class="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${profile.name}</h5>
+              <p class="card-text">
+              ${profile.description}
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+              <p>Place:<span> ${profile.place}</span></p>
+              <p>Capacity:<span> ${profile.capacity}</span></p>
+               
+              <p>Price:<span> $${profile.price}</span></p>
+        </div>
+      </div>
+`
+container.innerHTML = html;
 
-// Llamadas de funciones
-pintarTarjetas(events)
-crearChecks(events)
-
-
-// Funciones
-function superFiltro(){
-  let arrayFiltrado1 = filtrarPorTexto(events, input.value)
-  let arrayFiltrado2 = filtrarPorCategoria(arrayFiltrado1)
-  pintarTarjetas(arrayFiltrado2)
-}
-
-function pintarTarjetas(arrayDatos) {
-    if(arrayDatos.length == 0){
-      contenedor.innerHTML ="<div class ='tarjetaErrorUpcoming'><h3>¡No hay coincidencias!</h3></div>"
-      return
-    }
-
-    let tarjetas = '';
-  
-    arrayDatos.forEach(element => {
-      if(element.date > fechaActual){
-    
-      tarjetas += `
-      <div class="card" style="width: 18rem">
-    <img src=" ${element.image} "/>
-    <div class="card-body">
-      <h5 class="card-title">${element.name}</h5>
-      <p class="card-text">
-        ${element.description}
-      </p>
-      <span class="price">Price:$${element.price}</span>
-      <a href="./detail.html?id=${element._id}" class="btn-card"> Go somewhere</a>
-    </div>
-  </div>
-      `
-    }
-    });
-    contenedor.innerHTML = tarjetas;
-}
-
-function crearChecks(arrayDatos) {
-  let checks = ''
-  let categoriasRepetidas = arrayDatos.map(element => element.category)
-
-  let categorias = new Set(categoriasRepetidas.sort((a,b)=>{
-    if(a>b){
-      return 1
-  }
-    if(a<b){
-      return -1
-  }
-      return 0
-  }))
-  categorias.forEach(element =>{
-    checks += `<div class="form-check">
-    <input class="form-check-input" type="checkbox" id="${element}" value="${element}">
-    <label class="form-check-label" for="${element}">${element}</label>
-  </div>`
-  })
-
-  contenedorChecks.innerHTML = checks
-}
-
-function filtrarPorTexto(arrayDatos, texto) {
-  let arrayFiltrado = arrayDatos.filter(element => element.name.toLowerCase().includes(texto.toLowerCase()))
-  return arrayFiltrado
-
-}
-
-function filtrarPorCategoria(arrayDatos) {
-  let checkboxes = document.querySelectorAll("input[type='checkbox']")
-  console.log(checkboxes);
-  let arrayChecks = Array.from(checkboxes)
-  console.log(arrayChecks);
-  let checksChecked = arrayChecks.filter(check => check.checked)
-  console.log(checksChecked);
-  if(checksChecked.length == 0){
-    return arrayDatos
-  }
-  let checkValues = checksChecked.map(check => check.value)
-  console.log(checkValues);
-  let arrayFiltrado = arrayDatos.filter(element =>
-    checkValues.includes(element.category))
-    console.log(arrayFiltrado);
-    return arrayFiltrado
-
-
-}
+{/* <p>Assistance:<span> ${profile.assistance}</span></p> */}
--- js/past.js
+++ js/past.js
@@ -185,62 +185,108 @@ var data = data;
 const fechaActual = data.currentDate;
 const events = data.events;
 
-let upcomingCardArray = [];
-let pastCardArray = [];
 
-/* function eventCardsMain() {
-  for (let i = 0; i < events.length; i++) {
-    if (events[i].date > fechaActual) {
-      upcomingCardArray.push(events[i]);
-    } else {
-      pastCardArray.push(events[i]);
-    }
-  }
+// Constantes capturadas y variables
+const contenedor = document.getElementById('items-cards-past')
+const contenedorChecks = document.getElementById('checkbox-items')
+const input = document.querySelector('input')
+
+
+
+// Eventos
+input.addEventListener('input',superFiltro)
+
+contenedorChecks.addEventListener('change',superFiltro)
+
+
+// Llamadas de funciones
+pintarTarjetas(events)
+crearChecks(events)
+
+
+// Funciones
+function superFiltro(){
+  let arrayFiltrado1 = filtrarPorTexto(events, input.value)
+  let arrayFiltrado2 = filtrarPorCategoria(arrayFiltrado1)
+  pintarTarjetas(arrayFiltrado2)
 }
 
-eventCardsMain(); */
-/* console.log(upcomingCardArray); */
-/* console.log("__________________"); */
-/* console.log(pastCardArray); */
-
-/* function past(event) {
-  for (let i = 0; i < events.length; i++)
-    if (events[i].date > fechaActual) {
-      upcomingCardArray.push(events[i]);
-    } else {
-      pastCardArray.push(events[i]);
+function pintarTarjetas(arrayDatos) {
+    if(arrayDatos.length == 0){
+      contenedor.innerHTML = "<div class ='tarjetaErrorPast'><h3>¡No hay coincidencias!</h3></div>"
+      return
     }
-} */
 
-function paintDOM(events) {
-  let body = ``;
-
-  const tagToUpdate = document.getElementById("items-cards-past");
-  console.log("tagToUpdate", tagToUpdate);
-
-  for (let i = 0; i < events.length; i++) {
-    if (events[i].date > fechaActual) {
-      upcomingCardArray.push(events[i]);
-    } else {
-      pastCardArray.push(events[i]);
-
-      body += `
-    <div class="card" style="width: 18rem">
-    <img src=" ${events[i].image} "/>
+    let tarjetas = '';
+  
+    arrayDatos.forEach(element => {
+      if(element.date < fechaActual){
+    
+      tarjetas += `
+      <div class="card" style="width: 18rem">
+    <img src=" ${element.image} "/>
     <div class="card-body">
-      <h5 class="card-title">${events[i].name}</h5>
+      <h5 class="card-title">${element.name}</h5>
       <p class="card-text">
-        ${events[i].description}
+        ${element.description}
       </p>
-      <span class="price">Price:$${events[i].price}</span>
-      <a href="./detail.html" class="btn-card"> Go somewhere</a>
+      <span class="price">Price:$${element.price}</span>
+      <a href="./detail.html?id=${element._id}" class="btn-card"> Go somewhere</a>
     </div>
   </div>
-    `;
+      `
     }
-  }
-  tagToUpdate.innerHTML = body;
+    });
+    contenedor.innerHTML = tarjetas;
 }
 
-paintDOM(events);
+function crearChecks(arrayDatos) {
+  let checks = ''
+  let categoriasRepetidas = arrayDatos.map(element => element.category)
+
+  let categorias = new Set(categoriasRepetidas.sort((a,b)=>{
+    if(a>b){
+      return 1
+  }
+    if(a<b){
+      return -1
+  }
+      return 0
+  }))
+  categorias.forEach(element =>{
+    checks += `<div class="form-check">
+    <input class="form-check-input" type="checkbox" id="${element}" value="${element}">
+    <label class="form-check-label" for="${element}">${element}</label>
+  </div>`
+  })
+
+  contenedorChecks.innerHTML = checks
+}
+
+function filtrarPorTexto(arrayDatos, texto) {
+  let arrayFiltrado = arrayDatos.filter(element => element.name.toLowerCase().includes(texto.toLowerCase()))
+  return arrayFiltrado
+
+}
+
+function filtrarPorCategoria(arrayDatos) {
+  let checkboxes = document.querySelectorAll("input[type='checkbox']")
+  console.log(checkboxes);
+  let arrayChecks = Array.from(checkboxes)
+  console.log(arrayChecks);
+  let checksChecked = arrayChecks.filter(check => check.checked)
+  console.log(checksChecked);
+  if(checksChecked.length == 0){
+    return arrayDatos
+  }
+  let checkValues = checksChecked.map(check => check.value)
+  console.log(checkValues);
+  let arrayFiltrado = arrayDatos.filter(element =>
+    checkValues.includes(element.category))
+    console.log(arrayFiltrado);
+    return arrayFiltrado
+
+
+}
+
 
